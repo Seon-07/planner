c16ca965177557dfb7b605952e789f978cfbd371
seon <tjswlghks0722@naver.com>
2024-03-07 12:09:32 +0900

    create planner
    플래너 생성

---
+++ src/test/java/com/seon/planer/PlanerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seon.planer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
